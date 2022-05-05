4008b732266f2c8f78402e131ebc4bd670a382a5
Kemal DAVUT <mustafakemaldavut@gmail.com>
2022-05-05 13:55:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+let cards = []
+for (let i = 0; i < names.length; i++) {
+cards.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+}
+return cards
+}
+
+function countDown(numbers){
+    while (numbers >= 0){
+        console.log(numbers);
+        numbers--;
+    }
+}
+countDown(10);
